68a7ac1eace69143a5f11e8e5617d3b34a882343
Benjamin Goff <bgoff@mainerobotics.org>
2021-03-19 09:49:53 -0400

    Add project files.

---
+++ CsvEmployeeWriter/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsvEmployeeWriter
+{
+    class Employee
+    {
+        public int EmpNum { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+}
---
+++ CsvEmployeeWriter/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+namespace CsvEmployeeWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int END = -1;
+            const string DELIM = ",";
+            const string FILENAME = "EmployeeData.txt";
+
+            Employee emp = new Employee();
+            FileStream outFile = new
+                FileStream(FILENAME, FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(outFile);
+            string input;
+            int outNum;
+            double outDec;
+
+            Console.Write("Enter employee number or {0} to quit", END);
+            input = Console.ReadLine();
+
+            if(int.TryParse(input,out outNum))
+            {
+                emp.EmpNum = outNum;
+            }
+            else
+            {
+                emp.EmpNum = -1;
+            }
+
+            try
+            {
+                while (emp.EmpNum != END)
+                {
+                    Console.Write("Enter last name>> ");
+                    emp.Name = Console.ReadLine();
+
+                    Console.Write("Enter salary>> ");
+                    input = Console.ReadLine();
+                    if (double.TryParse(input, out outDec))
+                    {
+                        emp.Salary = outDec;
+                    }
+
+                    writer.WriteLine("{1} {0} {2} {0} {3}"
+                        , DELIM, emp.EmpNum, emp.Name, emp.Salary);
+                    Console.Write("Enter employee number or {0} to quit", END);
+                    input = Console.ReadLine();
+
+                    if (int.TryParse(input, out outNum))
+                    {
+                        emp.EmpNum = outNum;
+                    }
+                    else
+                    {
+                        emp.EmpNum = -1;
+                    }
+                }
+            } finally
+            {
+                writer.Close();
+                outFile.Close();
+            }
+            
+
+             
+        }
+    }
+}
